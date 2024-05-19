ed7c495e87235f755ae3106bb6978921fc3c656e
אלעד ביבי <ebibi@g.jct.ac.il>
2024-05-19 20:13:33 +0300

    commit test for point class

---
+++ src/unittests/PointTest.java
@@ -0,0 +1,24 @@
+package unittests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void distance() {
+    }
+
+    @Test
+    void distanceSquared() {
+    }
+}
